19d96a4bcce0707fc1fa1d8c96a93860ab8012d7
DWT <2635246427@qq.com>
2022-10-26 22:35:23 +0800

    调整

--- src/App.vue
+++ src/App.vue
@@ -16,4 +16,13 @@ export default defineComponent({
 })
 </script>
 
-<style lang="less"></style>
+<style lang="less">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

5fd0809de54e62154eab4c5c6fbecabff147dfd5
DWT <2635246427@qq.com>
2022-10-26 23:00:26 +0800

    引入elementplus

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <img alt="Vue logo" src="./assets/logo.png" />
+    <h2>{{ $store.state.name }}</h2>
     <router-link to="/login">登录</router-link>
     <router-link to="/main">首页</router-link>
     <router-view></router-view>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import { globalRegister } from './global'
 
-createApp(App).use(router).mount('#app')
+import router from './router'
+import store from './store'
+
+createApp(App).use(router).use(store).use(globalRegister).mount('#app')

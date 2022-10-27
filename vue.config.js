d17ac385016b528d902d2d8eae8e89c235a8f769
DWT <2635246427@qq.com>
2022-10-27 20:48:24 +0800

    登入跳转到主页

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,7 @@ module.exports = {
   devServer: {
     proxy: {
       '^/api': {
-        target: 'http://152.136.185.210:5000',
+        // target: 'http://152.136.185.210:5000',
         pathRewrite: {
           '^/api': ''
         },
